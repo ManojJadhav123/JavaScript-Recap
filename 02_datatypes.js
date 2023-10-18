e6f3e3d47e65904c40432abcd21b7101c2401cb6
Manoj Jadhav <63336783+ManojJadhav123@users.noreply.github.com>
2023-10-18 03:48:21 -0700

    new file

---
+++ 02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict" //treat all JS code as newer version
+
+let name = "manoj";
+let age = 23;
+let isloggedIn = false;
+let state;
+
+//number
+//bigInt
+//string
+//boolean
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
