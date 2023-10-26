4f91de5327a8ee69ba78156329267a8884eb91a0
Manoj Jadhav <63336783+ManojJadhav123@users.noreply.github.com>
2023-10-26 02:19:19 -0700

    random color project

---
+++ 07_mini_projects/5-unlimitedColors/script.js
@@ -0,0 +1,30 @@
+//generate a random color
+
+const randomColor = function(){
+    const hex = "0123456789ABCDEF";
+    let color = "#";
+    for(let i = 0; i<6; i++){
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+let intervalId;
+
+const startchangingColor = function(){
+    
+    if(!intervalId){
+        intervalId = setInterval(changeBackGroundColor , 1000)
+    }
+
+    function changeBackGroundColor(){
+        document.body.style.backgroundColor = randomColor();
+    }
+}
+const stopchangingColor = function(){
+    clearInterval(intervalId);
+    intervalId = null;
+}
+
+document.querySelector("#start").addEventListener('click', startchangingColor);
+document.querySelector("#stop").addEventListener('click', stopchangingColor);
